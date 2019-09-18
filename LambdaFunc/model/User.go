40e973b63d292a6e650f2fd80f7dd1b6858d3284
doc-jones <37349558+doc-jones@users.noreply.github.com>
2019-09-18 03:08:47 -0400

    initial commit

---
+++ LambdaFunc/model/User.go
@@ -0,0 +1,17 @@
+package model
+
+import "github.com/jinzhu/gorm"
+
+type User struct {
+	gorm.Model
+	ID uint
+	Email string `json:"email"`
+	SlackID string `json:"slack"`
+	GithubID string `json:"github"`
+	Firstname  string `json:"firstname"`
+	Lastname  string `json:"lastname"`
+	Cohort string `json:"cohort"`
+	Track string `json:"track"`
+	Backend string `json:"backend"`
+	Timezone string `json:"timezone"`
+}
