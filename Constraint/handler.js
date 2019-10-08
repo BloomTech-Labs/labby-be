b88967e33481946b9edc7229eea69c827acd43f7
efrain <efarinva98@gmail.com>
2019-10-08 16:48:06 -0400

    added variables and began

--- Constraint/handler.js
+++ Constraint/handler.js
@@ -1,18 +1,58 @@
 'use strict';
 
 import * as kiwi from 'kiwi.js';
+import { Variable } from 'kiwi.js';
+
+const pgSettings = {
+  host: "labbydatabase.cu5flbcmyfuw.us-east-1.rds.amazonaws.com",
+  user: "postgres",
+  password: "password",
+  port: 5432,
+  database: "postgres"
+};
+// connects to aws database
+const knex = require("knex")({
+  client: "pg",
+  connection: pgSettings
+});
 
 
-module.exports.kiwi = async event => {
+exports.kiwi = async (event, context, callback) => {
+  let projectroles = await knex("project_roles")
+  let projects = await knex("projects")
+  
+  let solver = new kiwi.Solver();
+  let personid = new kiwi.Variable()
+  
+  projects.forEach(item => {
+    let currentproject = item.id 
+    let placeholder = []
+    
+    projectroles.map(e => {
+      if(e.project_id == currentproject){
+        placeholder.push(e)
+      }
+
+    })
+    let ux = []
+    let ds = []
+    let web = []
+    // let ios = []
+    // let android = []
+    placeholder.map(e => {
+      if(e.role_id == 8){
+        ux.push(e)
+      }else if(e.role_id == 4){
+        ds.push(e)
+      }else if (e.role_id == 5){
+        web.push(e)
+      }
+    })
+    
+  })
+
   return {
-    statusCode: 200,
-    body: JSON.stringify(
-     
-      null,
-      2
-    ),
+
   };
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
