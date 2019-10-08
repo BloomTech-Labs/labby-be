a729f6376bdc5c2ad890028c3c9425d16f7b6152
Laura Daugherty <lauradaugherty6@gmail.com>
2019-10-07 20:59:30 -0700

    spelling changes & TODO items

--- Sorting/handler.js
+++ Sorting/handler.js
@@ -1,6 +1,7 @@
-// all handlers/functions. functions have to be declared inside the .yml before decalring them here 
+// all handlers/functions. functions have to be declared inside the .yml before declaring them here 
 "use strict";
 // pgsettings connections settings 
+//TODO: import db.js for pgSettings
 const pgSettings = {
   host: "labbydatabase.cu5flbcmyfuw.us-east-1.rds.amazonaws.com",
   user: "postgres",
@@ -29,8 +30,9 @@ exports.getAllProjects = async (event, context, callback) => {
     });
 };
 // post a project
+//TODO: ADD IN ERROR CASES 
 exports.postProject = async (event, context, callback) => {
-  // decalaring varaibles 
+  // declaring variables 
 
   const body = JSON.parse(event.body);
   const postBody = { ...req.body };
@@ -159,3 +161,84 @@ exports.projectRoles = async (event, context, callback) => {
     });
   }
 };
+
+
+// const projectRoles = async (event, context, callback) => {
+//   console.log("Starting Project Roles Function...");
+
+//   const projects = await knex("projects").select("projects.id");
+
+//   const projectsMap = projects.map(project => {
+//     return { project_id: project.id };
+//   });
+//   // /\grabbing project-roles
+//   let projectRoles = await knex("project_roles");
+//   // we need filled projects for the loop
+//   let filledProjects = 0;
+//   // a empty array so we can push all our role.ids
+//   const placeholder = [];
+
+
+//   await knex("project_roles").then(async res => {
+//     // looping through projects
+//     projects.forEach(project => {
+//       let d = Math.round(projectRoles.length / projectsMap.length);
+//       console.log("during forEach", d);
+
+//       for (let i = 0; i < d; i++) {
+//         let current = projectRoles[i + filledProjects * d];
+//         // checking if the current item is null and if it is we move to the next if statment 
+//         if (projectRoles[i + filledProjects * d] != null) {
+//           // checking if the current item is equal to d which is th amount of groups
+//           if (i == d - 1) {
+
+//             filledProjects++;
+//           }
+//           placeholder.push({ id: current.id, project_id: project.id });
+//         }
+//       }
+//     });
+// //  putting/updating the placeholder array into the project_roles
+//     let updatedProjects = placeholder.map(async p => {
+//       console.log("updatedProjects", placeholder);
+//       return await knex("project_roles")
+//         .where({ id: p.id })
+//         .update({ project_id: p.project_id });
+//     });
+//   });
+  
+//   try {
+//     const allProjects = await knex("project_roles");
+//     knex.client.destroy();
+//     return callback(null, {
+//       statusCode: 200,
+//       body: JSON.stringify(allProjects)
+//     });
+//   } catch (err) {
+//     return callback(null, {
+//       statusCode: 500,
+//       body: JSON.stringify(err.message)
+//     });
+//   }
+// };
+
+// projectRoles()
+
+const getAllProjects = async (event, context, callback) => {
+  console.log("inside get all projects")
+  await knex("projects")
+    .then(projects => {
+      knex.client.destroy();
+      console.log("callback", callback)
+      return callback(null, {
+        statusCode: 200,
+        body: JSON.stringify(projects)
+      });
+    })
+    .catch(err => {
+      knex.client.destroy();
+      return callback(err.message);
+    });
+};
+
+getAllProjects()
