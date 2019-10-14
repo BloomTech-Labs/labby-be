0ef800baf6e28c1f68898c5dce744a679d3f40de
Laura Daugherty <lauradaugherty6@gmail.com>
2019-10-13 18:50:35 -0700

    adding basic test and getAllProjects test

--- Sorting/handler_test_helper.spec.js
+++ Sorting/handler_test_helper.spec.js
@@ -1,2 +1,22 @@
 const app = require('./handler')
 const supertest = require('supertest')
+
+
+describe('handler_test_helper Get calls', () => {
+  let request
+  beforeEach(() => {
+    request = supertest()
+  })
+
+  test('getAllProjects should return 200', async done => {
+    const projectsResp = await app.getAllProjects(null, null, (error, resp) => {
+      expect(error).toBe(null)
+      expect(resp.statusCode).toBe(200)
+      return resp
+    })
+
+    expect(projectsResp).not.toBe(undefined)
+    expect(projectsResp.statusCode).toBe(200);
+    done()
+  })
+
